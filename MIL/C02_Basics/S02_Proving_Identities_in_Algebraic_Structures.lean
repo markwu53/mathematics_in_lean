4d2343080f1ec4704c20265da4253013c476170c
markwu53 <markwu53@users.noreply.github.com>
2026-06-02 23:22:43 +0000

    S01

--- MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
+++ MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -53,13 +53,16 @@ theorem neg_add_cancel_left (a b : R) : -a + (a + b) = b := by
 
 -- Prove these:
 theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
-  sorry
+  --sorry
+  rw [add_assoc, add_neg_cancel, add_zero]
 
 theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
-  sorry
+  --sorry
+  rw [← neg_add_cancel_left a b, h, neg_add_cancel_left]
 
 theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
-  sorry
+  --sorry
+  rw [← neg_add_cancel_left b a, add_comm, add_comm b, add_assoc, add_neg_cancel, add_zero]
 
 theorem mul_zero (a : R) : a * 0 = 0 := by
   have h : a * 0 + a * 0 = a * 0 + 0 := by
@@ -143,4 +146,3 @@ theorem mul_inv_rev (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
 end MyGroup
 
 end
-
